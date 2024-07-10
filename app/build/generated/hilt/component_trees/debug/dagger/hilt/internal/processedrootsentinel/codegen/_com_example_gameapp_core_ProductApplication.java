b4c04f62a306a7a94cf85db7d612f48250a90acd
sajjadfatehi <sajad.fatehi@snapp.cab>
2024-07-10 16:42:15 +0330

    remove useless fils

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_gameapp_core_ProductApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.gameapp.core.ProductApplication"
-)
-public final class _com_example_gameapp_core_ProductApplication {
-}
